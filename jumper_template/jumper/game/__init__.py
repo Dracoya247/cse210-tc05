4fa55c920a8d9b495b23d61bd56d197f78cf7d5a
Dracoya247 <56097912+Dracoya247@users.noreply.github.com>
2021-02-03 18:33:13 -0800

    Update __init__.py

--- jumper_template/jumper/game/__init__.py
+++ jumper_template/jumper/game/__init__.py
@@ -1,3 +1,20 @@
 """
 The game package contains specific classes for playing Jumper.
 """
+
+class director():
+    
+    pass
+
+
+class jumper():
+
+    pass
+
+class word():
+
+    pass
+
+class console():
+
+    pass
