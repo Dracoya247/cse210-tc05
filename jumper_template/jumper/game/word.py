5fd0eaae753bbc8a5455b0871b799abb2c8fd69d
Ezra Vizcarra <vizcarraezra@yahoo.com>
2021-02-07 00:01:35 -0700

    Added a comment, providing additional insight to the class.

--- jumper_template/jumper/game/word.py
+++ jumper_template/jumper/game/word.py
@@ -24,3 +24,25 @@ class Word():
             if l == "_":
                 return False
         return True
+
+"""
+An idea/outline to how it could also be:
+import random
+
+
+    #Random words
+word_list = 'ant baboon badger bat bear beaver camel cat clam cobra cougar\
+        coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk\
+        lion lizard llama mole monkey moose mouse mule newt otter owl panda\
+        parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep\
+        skunk sloth snake spider stork swan tiger toad trout turkey turtle\
+        weasel whale wolf wombat zebra rainbow computer science programming\
+        python player condition reverse water board geeks'.split()
+
+def get_random(word_list):
+        word_index = random.randint(0, len(word_list) - 1)
+        return word_list [word_index]
+
+secret_word = get_random(word_list)
+print(secret_word)
+"""
