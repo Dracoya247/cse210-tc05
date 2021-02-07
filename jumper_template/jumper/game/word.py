46516a02860afd7293363bb3201a41ac7bb5ebf6
Michael <rom19011@byui.edu>
2021-02-06 23:35:05 -0700

    Minor changes to word.py

--- jumper_template/jumper/game/word.py
+++ jumper_template/jumper/game/word.py
@@ -1,12 +1,26 @@
+from Math import * 
+
 class Word():
 
+    #Random words
+    word_list = ['rainbow', 'computer', 'science', 'programming', 
+            'python', '', 'player', 'condition', 
+            'reverse', 'water', 'board', 'geeks'] 
 
-import random 
+    def get_word(self):
 
-#Random words
-word_list = ['rainbow', 'computer', 'science', 'programming', 
-         'python', '', 'player', 'condition', 
-         'reverse', 'water', 'board', 'geeks'] 
+        return word_list[int(Math.random() * 11)]        
 
-get_word =(word_list)
-    pass
+    def guess(self, letter, word):
+
+        for l in word:
+            if l == letter:
+                return True
+        return False
+
+    def word_done(self, puzzle):
+
+        for l puzzle:
+            if l == "_":
+                return False
+        return True
