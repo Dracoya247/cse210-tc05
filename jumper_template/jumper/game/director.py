26325b2b8e8a6f94d53b0165a8a11d423723ce4a
Ezra Vizcarra <vizcarraezra@yahoo.com>
2021-02-06 22:13:38 -0700

    Still needs a bit of work.

--- jumper_template/jumper/game/director.py
+++ jumper_template/jumper/game/director.py
@@ -1,3 +1,38 @@
-class Director():
+from game.console import Console
+from game.jumper import Jumper
+from game.word import Word
 
-    pass
+class Director():
+    """This class directs(controls) the sequence of play. This should
+    be easy for the programmer to understand the sequence of the game."""
+
+    def __init__(self):
+        """The class constructor.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.console = Console()
+        self.jumper = Jumper()
+        self.keep_playing = True
+        self.word = Word()
+
+    def start_game(self):
+        """Initiates the game in a loop controlling the sequence of play.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        pass
+
+    def do_updates(self):
+        pass
+
+    def do_outputs(self):
+        pass
