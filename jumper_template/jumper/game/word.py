ab536c2316d1af29ce2f2d9a6b7927785c03d82c
Michael <rom19011@byui.edu>
2021-02-06 22:22:37 -0700

    Minor changes/revision to word.py

---
+++ jumper_template/jumper/game/word.py
@@ -0,0 +1,12 @@
+class Word():
+
+
+import random 
+
+#Random words
+word_list = ['rainbow', 'computer', 'science', 'programming', 
+         'python', '', 'player', 'condition', 
+         'reverse', 'water', 'board', 'geeks'] 
+
+get_word =(word_list)
+    pass
